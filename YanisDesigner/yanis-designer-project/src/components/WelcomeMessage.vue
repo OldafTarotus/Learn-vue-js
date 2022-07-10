b21dd6409a2856333a1a3de98866a7fdfec61901
OldafTarotus <Mis.Ahmed.Naser@gmail.com>
2022-07-11 00:58:45 +0200

    component

---
+++ YanisDesigner/yanis-designer-project/src/components/WelcomeMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome-message">
+    <h2>Welcome to vuejs Course</h2>
+    <p>Oldaf Trying to be a professional fullstack Developar</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `Hiho`,
+};
+</script>
+
+<style>
+h2 {
+  font-size: 40px;
+}
+</style>
